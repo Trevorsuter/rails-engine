99795c5d390f75ace03fcb5de265394ef741a11e
Trevor Suter <Trevorsuter@icloud.com>
2021-04-07 09:35:26 -0600

    refactored to application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  
+  def per_page
+    params[:per_page] || 20
+  end
+
+  def page
+    if params[:page] && params[:page].to_i > 0
+      (params[:page].to_i - 1) * per_page.to_i
+    else
+      0
+    end
+  end
 end
